aeadeed18e841c5c4965a5f9ad797db77c6a0a73
aldomq <58537416+aldomq@users.noreply.github.com>
2026-03-11 13:13:50 -0600

    Mysql fixed
    Added service, repository and domain.
    Fixed controller and application.properties

---
+++ src/main/java/com/tienda/service/CategoriaService.java
@@ -0,0 +1,30 @@
+
+package com.tienda.service;
+import com.tienda.domain.Categoria;
+import java.util.List;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.tienda.repository.CategoriaRepository;
+
+
+@Service
+public class CategoriaService {
+    private final CategoriaRepository categoriaRepository;
+    
+    public CategoriaService(CategoriaRepository categoriaRepository){
+        this.categoriaRepository = categoriaRepository;
+    
+    }
+    
+    @Transactional(readOnly=true)
+    public List<Categoria> getCategorias(boolean activo){
+        if (activo) {
+            return categoriaRepository.findByActivoTrue();
+        } else {
+            return categoriaRepository.findAll();
+        
+        }
+    
+    }
+            
+}
