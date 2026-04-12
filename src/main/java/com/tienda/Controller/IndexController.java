7f6098333646badcf6908e47bf0f53fcea673696
aldomq <58537416+aldomq@users.noreply.github.com>
2026-04-12 17:46:44 -0600

    code fixed

--- src/main/java/com/tienda/Controller/IndexController.java
+++ src/main/java/com/tienda/Controller/IndexController.java
@@ -2,6 +2,7 @@ package com.tienda.Controller;
 
 import com.tienda.service.CategoriaService;
 import com.tienda.service.ProductoService;
+import java.util.Collections;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,34 +17,33 @@ public class IndexController {
     public IndexController(CategoriaService categoriaService, ProductoService productoService) {
         this.categoriaService = categoriaService;
         this.productoService = productoService;
-
     }
 
     @GetMapping("/")
     public String listado(Model model) {
         var productos = productoService.getProductos(true);
-        model.addAttribute("productos", productos);
         var categorias = categoriaService.getCategorias(true);
+
+        model.addAttribute("productos", productos);
         model.addAttribute("categorias", categorias);
 
-        return "/index";
+        return "index";
     }
 
-    @GetMapping("/consultas/(idCategoria)")
-    public String listado(@PathVariable("idCategoria") Integer idCategoria, Model model) {
+    @GetMapping("/consultas/{idCategoria}")
+    public String listadoCategoria(@PathVariable("idCategoria") Integer idCategoria, Model model) {
         var categoriaOpt = categoriaService.getCategoria(idCategoria);
-        if (categoriaOpt.isEmpty()) {
-            //No hay productos
-            model.addAttribute("productos", java.util.Collections.EMPTY_LIST);
-        } else {
+
+        if (categoriaOpt.isPresent()) {
             var categoria = categoriaOpt.get();
-            var productos = categoria.getProductos();
-            model.addAttribute("productos", productos);
+            model.addAttribute("productos", categoria.getProductos());
+        } else {
+            model.addAttribute("productos", Collections.emptyList());
         }
 
         var categorias = categoriaService.getCategorias(true);
         model.addAttribute("categorias", categorias);
-        return "/index";
+
+        return "index";
     }
-//p
 }
